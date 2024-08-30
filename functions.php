c69570db1180fcf4699d8bead8dab8e93724a10d
Rukmagat Kandel <101416363+sunder55@users.noreply.github.com>
2024-08-30 12:57:58 +0545

    Added featured image to the domain list

--- functions.php
+++ functions.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Functions and definitions
  *
@@ -68,13 +69,38 @@ function prefix_disable_gutenberg($current_status, $post_type)
  * This action hook allows to add a new empty column
  */
 add_filter('manage_domain_posts_columns', 'misha_featured_image_column');
-function misha_featured_image_column( $column_array ) {
+function misha_featured_image_column($column_array)
+{
 
     // I want to add my column at the beginning, so I use array_slice()
     // in other cases $column_array['featured_image'] = 'Featured Image' will be enough
-    $column_array = array_slice( $column_array, 0, 1, true )
-    + array('featured_image' => 'Featured Image') // our new column for featured images
-    + array_slice( $column_array, 1, NULL, true );
+    $column_array = array_slice($column_array, 0, 1, true)
+        + array('featured_image' => 'Featured Image') // our new column for featured images
+        + array_slice($column_array, 1, NULL, true);
 
     return $column_array;
+}
+
+/*
+ * This hook will fill our column with data
+ */
+add_action('manage_posts_custom_column', 'misha_render_the_column', 10, 2);
+function misha_render_the_column($column_name, $post_id)
+{
+
+    if ($column_name == 'featured_image') {
+
+        // if there is no featured image for this post, print the placeholder
+        if (has_post_thumbnail($post_id)) {
+
+            $thumb_id = get_post_thumbnail_id($post_id);
+           
+            echo '<img data-id="' . $thumb_id . '" src="' . wp_get_attachment_url($thumb_id) . '" style="width:40px; height:40px;" />';
+        } 
+         else {
+
+            // // data-id should be "-1" I will explain below
+            // echo '<img data-id="-1" src="' . get_stylesheet_directory_uri() . '/assets/image/wstr-placeholder.webp" />';
+        }
+    }
 }
