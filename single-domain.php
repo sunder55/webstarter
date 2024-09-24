2fca070fdda9ba39c79cb9613f4619ee36e34658
Code Pixelz Media <arpanadhikari63@gmail.com>
2024-09-24 10:18:44 +0545

    single-domain page sections added

--- single-domain.php
+++ single-domain.php
@@ -1,10 +1,12 @@
-<?php
-wp_head();
-block_header_area()
-?>
+<header class="wp-block-template-part site-header">
+    <?php echo do_blocks('<!-- wp:template-part {"slug":"header"} /-->'); ?>
+</header>
+<?php wp_head(); ?>
 
 <div class="single-container ws-container">
+    <div class="arpan"></div>
     <?php
+    // echo do_shortcode('[wstr-multicurrency]');
     // Start the Loop.
     while (have_posts()):
         the_post();
@@ -29,7 +31,7 @@ block_header_area()
         $category_id = $category[0]->term_id;
         $cat_image_id = get_term_meta($category_id, 'taxonomy-image-id', true);
         $cat_image_url = wp_get_attachment_url($cat_image_id);
-
+        $tags = get_the_terms(get_the_ID(), 'domain_tag');
         $similar_domain_args = array(
             'post_type' => 'domain',
             'fields' => 'ids',
@@ -37,55 +39,19 @@ block_header_area()
             'tax_query' => array(
                 array(
                     'taxonomy' => 'domain_cat',
-                    'field'    => 'term_id',
-                    'terms'    => $category_id,
+                    'field' => 'term_id',
+                    'terms' => $category_id,
                     'operator' => 'IN',
                 )
             )
         );
-    ?>
-        <div class="similar-industry-names-main">
-            <?php
-            $similar_domain_ids = get_posts($similar_domain_args);
-            foreach ($similar_domain_ids as $similar_domain_id) {
-                $similar_domain_title = get_the_title($similar_domain_id);
-                $featured_image_id = get_post_thumbnail_id($similar_domain_id);
-                $featured_image_url = wp_get_attachment_url($featured_image_id);
 
-                $logo_image_id = get_post_meta($post->ID, '_logo_image', true);
-                $logo_image_url = wp_get_attachment_url($logo_image_id);
-
-                $permalink = get_permalink($similar_domain_id);
-            ?>
-                <div class="similar-industry-names">
-                    <img style="height: 100px; width:100px;" src="<?php echo $logo_image_url ? $logo_image_url : $featured_image_url ?>">
-                    <h5><?php echo $similar_domain_title ?></h5>
-                    <?php echo get_wstr_price($similar_domain_id); ?>
-                </div>
-            <?php
-            }
-
-            ?>
-        </div>
-        <?php
-        $tags = get_the_terms(get_the_ID(), 'domain_tag');
-        ?>
-        <div class="domain-tag">
-            <h3>Related tags</h3>
-            <?php
-            foreach ($tags as $tag) {
-                $tag_id = $tag->term_id;
-                $tag_name = $tag->name;
-            }
-            ?>
-        </div>
-        <?php
         // $discount_percent = esc_html($domain['precentage_discount']);
         // $term_exist = isset($domain['term_exist']) ? (bool) $domain['term_exist'] : true; // Default to true if not set
-
+    
 
         // $currency = esc_html($domain['currency']);
-
+    
         $da = $pa = '';
         if ($da_pa) {
             $da_pa_split = explode('/', $da_pa);
@@ -107,17 +73,18 @@ block_header_area()
                 <div class="single_featured_image_footer ws_flex">
                     <span class="domain_online ws_flex gap_10 ai_center online">
                         <i class="fa-solid fa-comments"></i>
-                        <p>Online</p>
-                        <i class="fa-solid fa-circle"></i>
+                        <p>Online
+                            <i class="fa-solid fa-circle"></i>
+                        </p>
                     </span>
                     <a href="#">
                         <p>Message</p>
                     </a>
                 </div><?php
-                        if ($term_exist) {
-                            $output .= '<div class="premium_icon"><img src="/wp-content/plugins/card-block/images/diamond.png"
-                        alt="Diamond Icon" /></div>';
-                        } ?>
+                // if ($term_exist) {
+                $output .= '<div class="premium_icon"><img src="/wp-content/plugins/card-block/images/diamond.png"
+                            alt="Diamond Icon" /></div>';
+                // } ?>
                 <div class="ws_flex ai_center single_domain_meta_search">
                     <div class="single_domain_search">
                         <form>
@@ -134,10 +101,10 @@ block_header_area()
             <div class="domain-details">
                 <div class="ws_flex gap_20 ai_center p_relative">
                     <div><?php
-                            // if ((int) $discount_percent > 0) {
-                            //     $output .= '<div class="ws_discount_percent"> -' . $discount_percent . '%</div>';
-                            // }
-                            if ($logo): ?>
+                    // if ((int) $discount_percent > 0) {
+                    //     $output .= '<div class="ws_discount_percent"> -' . $discount_percent . '%</div>';
+                    // }
+                    if ($logo): ?>
                             <img src="<?php echo esc_url($logo); ?>" alt="<?php echo esc_attr($title); ?>" class="logo">
                         <?php elseif ($featured_image): ?>
                             <img src="<?php echo esc_url($featured_image); ?>" alt="<?php echo esc_attr($title); ?>"
@@ -156,14 +123,14 @@ block_header_area()
                             <?php
                             if (!empty($regular_price)) { ?>
                                 <p class="regular_price"><?php
-                                                            echo get_wstr_currency();
-                                                            echo get_wstr_regular_price(get_the_ID()); ?></p><?php
-                                                                                                            }
-                                                                                                            if (!empty($sale_price)) { ?>
+                                echo get_wstr_currency();
+                                echo get_wstr_regular_price(get_the_ID()); ?></p><?php
+                            }
+                            if (!empty($sale_price)) { ?>
                                 <p class="sale_price"><?php
-                                                                                                                echo get_wstr_currency();
-                                                                                                                echo get_wstr_sale_price(get_the_ID()); ?></p><?php
-                                                                                                                                                            } ?>
+                                echo get_wstr_currency();
+                                echo get_wstr_sale_price(get_the_ID()); ?></p><?php
+                            } ?>
                         </div>
                     </div>
                 </div>
@@ -192,7 +159,7 @@ block_header_area()
                 <div id="tab-1" class="tab-content current">
                     <h2 class="fw-600 margin_v_35">Domain Information</h2>
                     <div class="single_domain_progress_wrapper br_15">
-                        <h2 class="fw-600"><?php echo esc_html($title); ?></h2>
+                        <h4 class="fw-600"><?php echo esc_html($title); ?></h4>
                         <div class="ws_flex gap_20">
                             <div class="circular-progress page-trust">
                                 <div class="progress-text">
@@ -251,6 +218,59 @@ block_header_area()
                         the_content();
                         ?>
                     </div>
+                    <div class="single_domain_features ws_trending_cards margin_v_35">
+                        <h2>What You Get</h2>
+                        <div class="ws_flex gap_10">
+                            <div class="similar-industry-names ws-card-contents ws_flex single_domain_feature">
+                                <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/domain-name.png"
+                                    alt="Feature Image">
+                                <div class="ws-card-inner-contents">
+                                    <h5><?php echo $title ?></h5>
+                                    <div class="ws_card_price_wrapper ws_flex gap_10">
+                                        <p>Domain Name</p>
+
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="similar-industry-names ws-card-contents ws_flex single_domain_feature">
+                                <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/svg-file-icon.png"
+                                    alt="SVG Icon">
+                                <div class="ws-card-inner-contents">
+                                    <h5>SVG File & Copyright</h5>
+                                    <div class="ws_card_price_wrapper ws_flex gap_10">
+                                        <p class="">Logo Design</p>
+
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="similar-industry-names ws-card-contents ws_flex single_domain_feature">
+                                <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/technical-support.png"
+                                    alt="SVG Icon">
+                                <div class="ws-card-inner-contents">
+                                    <h5>Technical Support</h5>
+                                    <div class="ws_card_price_wrapper ws_flex gap_10">
+                                        <p>Free Technical Support</p>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="domain-tag">
+                        <h2>Related tags</h2>
+                        <ul class="related_tag_list_wrapper ws_flex gap_10"> <?php
+                        foreach ($tags as $tag) {
+                            ?>
+                                <li><?php
+                                $tag_id = $tag->term_id;
+                                $tag_name = $tag->name;
+                                echo $tag_name;
+                                ?>
+                                </li> <?php
+                        }
+                        ?>
+                        </ul>
+                    </div>
                 </div>
                 <div id="tab-2" class="tab-content">
                     <h2>Tab Two</h2>
@@ -268,31 +288,56 @@ block_header_area()
                         <h5>Fast and Secure Transfer</h5>
                     </div>
                     <div class="single_domain_highlights_card">
-                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/highlight-1.png"
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/highlight-2.png"
                             alt="Feature Image">
-                        <h5>Fast and Secure Transfer</h5>
+                        <h5>Free Technical Support</h5>
                     </div>
                     <div class="single_domain_highlights_card">
-                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/highlight-1.png"
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/highlight-3.png"
                             alt="Feature Image">
-                        <h5>Fast and Secure Transfer</h5>
+                        <h5>Professionally Crafter Logo</h5>
                     </div>
                     <div class="single_domain_highlights_card">
-                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/highlight-1.png"
-                            alt="Feature Image">
+                        <img src="<?php echo $cat_image_url ?> ">
                         <h5><?php echo $category_name ?></h5>
                     </div>
+                </div>
+                <div class="similar-industry-names-main ws_trending_cards margin_v_35">
+                    <h5>Similar Industry Names</h5>
+                    <?php
+                    $similar_domain_ids = get_posts($similar_domain_args);
+                    foreach ($similar_domain_ids as $similar_domain_id) {
+                        $similar_domain_title = get_the_title($similar_domain_id);
+                        $featured_image_id = get_post_thumbnail_id($similar_domain_id);
+                        $featured_image_url = wp_get_attachment_url($featured_image_id);
+                        $logo_image_id = get_post_meta($similar_domain_id, '_logo_image', true);
+                        $logo_image_url = wp_get_attachment_url($logo_image_id);
+                        $permalink = get_permalink($similar_domain_id);
+                        ?>
+                        <div class="similar-industry-names ws-card-contents ws_flex">
+                            <img src="<?php echo $logo_image_url ? $logo_image_url : $featured_image_url ?>">
+                            <div class="ws-card-inner-contents">
+                                <h5><?php echo $similar_domain_title ?></h5>
+                                <?php echo get_wstr_price($similar_domain_id); ?>
+                            </div>
+                        </div>
+                        <?php
+                    }
+                    ?>
                 </div>
             </div>
         </div>
 
-    <?php
+        <?php
     endwhile; // End the Loop.
     ?>
 </div>
 
-<?php
-// get_footer();
-wp_footer();
-block_footer_area()
-?>
+
+<footer class="wp-block-template-part site-footer">
+    <?php echo do_blocks('<!-- wp:template-part {"slug":"footer","tagName":"footer","className":"site-footer"} /-->'); ?>
+</footer>
+
+</div>
+
+<?php wp_footer(); ?>
