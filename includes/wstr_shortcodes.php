00b9cf81ef8bc7a1993cc0e0dc3287d3b341f82d
Rukmagat Kandel <101416363+sunder55@users.noreply.github.com>
2024-09-27 12:29:54 +0545

    Alternate image for highlights

--- includes/wstr_shortcodes.php
+++ includes/wstr_shortcodes.php
@@ -696,7 +696,8 @@ class wstr_shortcodes
                                 <h5>Professionally Crafter Logo</h5>
                             </div>
                             <div class="single_domain_highlights_card">
-                                <img src="<?php echo $cat_image_url ?> ">
+                                <?php var_dump ($cat_image_url); ?>
+                                <img src="<?php echo $cat_image_url ? $cat_image_url : get_stylesheet_directory_uri() . '/asstes/images/highlight-alternate.png'; ?> ">
                                 <h5><?php echo $category_name ?></h5>
                             </div>
                         </div>
